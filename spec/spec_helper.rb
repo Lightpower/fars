d701fce2a2c595214fa1adf10ed5dfb436ed29e7
Vitaly Bezkrovny <bva@aejis.eu>
2014-03-20 12:28:11 +0200

    * load required files only

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,14 +1,6 @@
-ENV['RACK_ENV'] = ENV['RAILS_ENV'] = 'test'
-
-require File.dirname(__FILE__) + '/../app/application'
-
-require 'rack/test'
-require 'database_cleaner'
 require 'rspec'
 require 'shoulda'
+require 'database_cleaner'
+require 'pry'
 
-FakeWeb.allow_net_connect = false
-
-FactoryGirl.find_definitions
-
-Dir["#{File.dirname(__FILE__)}/support/*.rb"].sort.each { |f| require f}
+require 'fars'
