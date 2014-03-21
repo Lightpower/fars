f7e9e280298afad2d3ec5faa41dd7c936bf261a6
Vitaly Bezkrovny <bva@aejis.eu>
2014-03-21 10:29:00 +0200

    + json test

--- spec/fars/base_model_serializer_spec.rb
+++ spec/fars/base_model_serializer_spec.rb
@@ -1,5 +1,21 @@
 require 'spec_helper'
 
 describe Fars::BaseModelSerializer do
+  before :all do
+    class Master < ActiveRecord::Base
+      has_many :slaves
+    end
+  end
+
+  context '#to_json' do
+    before :each do
+      @object = Master.create(id: 1, name: 'Object1', data: '123')
+      @json = {id: 1, name: 'Object1', data: '123'}.to_json
+    end
+
+    it 'represent as json' do
+      @object.to_json.should == @json
+    end
+  end
 
 end
