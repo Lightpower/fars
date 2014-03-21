d0daa0d206964287f2046447b1c2c8587e8ddb73
Vitaly Bezkrovny <bva@aejis.eu>
2014-03-21 10:28:42 +0200

    + load and cleaning database

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,3 +4,31 @@ require 'database_cleaner'
 require 'pry'
 
 require 'fars'
+
+require 'yaml'
+require 'active_record'
+
+RSpec.configure do |config|
+  config.color_enabled = true
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.after(:suite) do
+    DatabaseCleaner.strategy = :truncation
+    DatabaseCleaner.clean
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+end
+
+root      = File.dirname(__FILE__)
+db_config = YAML.load_file("#{root}/config/database.yml")
+ActiveRecord::Base.establish_connection(db_config)
