94a390093a7544e672eb20291cb2e9b5d11654a0
Alex Avoyants <shhavel@gmail.com>
2014-04-13 09:58:11 +0300

    Move module declaration before requires in lib/fars.rb

--- lib/fars.rb
+++ lib/fars.rb
@@ -1,12 +1,11 @@
 require 'active_record'
+
+module Fars; end
 require 'fars/base_object_serializer'
 require 'fars/base_model_serializer'
 require 'fars/base_collection_serializer'
 require 'fars/model_serializable'
 require 'fars/relation_serializable'
-
-module Fars
-end
 
 class ActiveRecord::Base
   include Fars::ModelSerializable
