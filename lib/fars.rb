94d6cdd9832b696be8f88023522b3ce89e192b9b
Alex Avoyants <shhavel@gmail.com>
2014-04-13 09:50:34 +0300

    Add module declaration into lib/fars.rb

--- lib/fars.rb
+++ lib/fars.rb
@@ -5,6 +5,9 @@ require 'fars/base_collection_serializer'
 require 'fars/model_serializable'
 require 'fars/relation_serializable'
 
+module Fars
+end
+
 class ActiveRecord::Base
   include Fars::ModelSerializable
 end
